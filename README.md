6db4c2f7409e5e1a6895718a21c9729de6d473d7
Kenny Jackelen <kennyjackelen@gmail.com>
2015-05-30 19:52:43 -0500

    separate configuration into JSON files

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # lend.js
 
 Automated ordering for Lending Club
+
+The script relies on the following environment variables:
+- `LENDINGCLUB_API_KEY` Your API key from Lending Club
+- `LENDINGCLUB_ACCOUNT_ID` Your Lending Club account ID (available on your account summary page)
+- `LENDINGCLUB_PORTFOLIO_ID` (Optional) The portfolio ID to add any new orders to
+
+Default configuration lives in `default.json`. You can copy this file to `config.json` to override the defaults.
+
+Learn more about Lending Club's API [here](https://www.lendingclub.com/developers/lc-api.action).
--- lend.js
+++ lend.js
@@ -4,53 +4,42 @@
 var request = require('request');
 var Q = require('q');
 var bunyan = require('bunyan');
+var nconf = require('nconf');
+
+// Load configuration from files
+nconf.file('custom', './config.json');
+nconf.file('default', './default.json');
+var config = nconf.get();
 
 // Secret configuration
 var MY_KEY = process.env.LENDINGCLUB_API_KEY;
 var MY_ACCOUNT_ID = process.env.LENDINGCLUB_ACCOUNT_ID;
 var PORTFOLIO_ID = process.env.LENDINGCLUB_PORTFOLIO_ID;
 
-// Configuration
-var PLACE_ORDERS = true;
-var API_VERS = 'v1';
-var LOAN_AMOUNT = 25;
-var THROTTLE_LENGTH = 1000;  // ms
-
 // Error logging
-var ERROR_LOG_FILE = 'log/error.log';
 var log = bunyan.createLogger({
             name: 'lend.js',
             streams: [{
-              path: ERROR_LOG_FILE
+              path: config.ERROR_LOG_FILE
             }]
           });
 
-// Filters
-var EXCLUDE_STATES = [ 'CA', 'FL', 'NV' ];
-var EXCLUDE_60_MO_LOANS = true;
-var EXCLUDE_GRADES = [ 'A', 'B', 'C' ];
-var MIN_MONTHS_EMPLOYED = 24;
-var MAX_INQUIRIES_LAST_6_MONTHS = 2;
-var INCLUDE_LOAN_PURPOSES = [ 'credit_card', 'debt_consolidation' ];  // Total list of purposes: 'credit_card', 'debt_consolidation', 'medical', 'educational', 'home_improvement', 'renewable_energy', 'small_business', 'wedding', 'vacation', 'moving', 'house', 'car', 'major_purchase', 'other'
-var EXCLUDE_PUBLIC_RECORDS = true;
-var MAX_DELINQUENCIES_LAST_2_YEARS = 0;
-var MIN_OPEN_CREDIT_LINES = 2;
-var MAX_OPEN_CREDIT_LINES = 19;
-var REQUIRE_REVOLVING_BALANCE_LESS_THAN_LOAN_AMOUNT = true;
-
+// Important to call this between requests since Lending Club
+// only allows one request per second.
 function throttle() {
   var deferred = Q.defer();
   var argsToPassAlong = arguments;
-  setTimeout( function() { deferred.resolve.apply( this, argsToPassAlong ); }, THROTTLE_LENGTH );
+  setTimeout( function() { deferred.resolve.apply( this, argsToPassAlong ); }, config.THROTTLE_LENGTH );
   return deferred.promise;
 }
 
+// Calls Lending Club's API to get all available listings
 function getAvailableListings() {
   var deferred = Q.defer();
   var lendingData = {};
 
   var options = {
-    url: 'https://api.lendingclub.com/api/investor/' + API_VERS + '/loans/listing?showAll=true',
+    url: 'https://api.lendingclub.com/api/investor/' + config.API_VERS + '/loans/listing?showAll=true',
     headers: {
       Authorization: MY_KEY,
       Accept: 'application/json'
@@ -75,11 +64,12 @@ function getAvailableListings() {
   }
 }
 
+// Calls Lending Club's API to get your account balance
 function getMyAccountBalance( lendingData ) {
   var deferred = Q.defer();
 
   var options = {
-    url: 'https://api.lendingclub.com/api/investor/' + API_VERS + '/accounts/' + MY_ACCOUNT_ID + '/availablecash',
+    url: 'https://api.lendingclub.com/api/investor/' + config.API_VERS + '/accounts/' + MY_ACCOUNT_ID + '/availablecash',
     headers: {
       Authorization: MY_KEY,
       Accept: 'application/json'
@@ -104,11 +94,13 @@ function getMyAccountBalance( lendingData ) {
   }
 }
 
+// Calls Lending Club's API to get the list of loans you've
+// already invested in
 function getMyLoans( lendingData ) {
   var deferred = Q.defer();
 
   var options = {
-    url: 'https://api.lendingclub.com/api/investor/' + API_VERS + '/accounts/' + MY_ACCOUNT_ID + '/notes',
+    url: 'https://api.lendingclub.com/api/investor/' + config.API_VERS + '/accounts/' + MY_ACCOUNT_ID + '/notes',
     headers: {
       Authorization: MY_KEY,
       Accept: 'application/json'
@@ -138,9 +130,10 @@ function getMyLoans( lendingData ) {
   }
 }
 
+// Filters out loans that don't meet your conditions
 function filterListings( lendingData ) {
   var deferred = Q.defer();
-  var loanCount = Math.floor( lendingData.balance / LOAN_AMOUNT );
+  var loanCount = Math.floor( lendingData.balance / config.LOAN_AMOUNT );
   lendingData.filteredListings = lendingData.listings.filter( filterOneListing );
   lendingData.filteredListings.sort( sortListings );
   lendingData.loansToOrder = lendingData.filteredListings.slice( 0, loanCount );
@@ -150,37 +143,38 @@ function filterListings( lendingData ) {
   return deferred.promise;
 
   function filterOneListing( listing ) {
+    var filters = config.FILTERS;
     if ( lendingData.alreadyInvested[ listing.id ] ) {
       return false;
     }
-    if ( EXCLUDE_STATES.lastIndexOf( listing.addrState ) > -1 ) {
+    if ( filters.EXCLUDE_STATES.lastIndexOf( listing.addrState ) > -1 ) {
       return false;
     }
-    if ( EXCLUDE_60_MO_LOANS && ( listing.term === 60 ) ) {
+    if ( filters.EXCLUDE_60_MO_LOANS && ( listing.term === 60 ) ) {
       return false;
     }
-    if ( EXCLUDE_GRADES.lastIndexOf( listing.grade ) > -1 ) {
+    if ( filters.EXCLUDE_GRADES.lastIndexOf( listing.grade ) > -1 ) {
       return false;
     }
-    if ( listing.empLength < MIN_MONTHS_EMPLOYED ) {
+    if ( listing.empLength < filters.MIN_MONTHS_EMPLOYED ) {
       return false;
     }
-    if ( listing.inqLast6Mths > MAX_INQUIRIES_LAST_6_MONTHS ) {
+    if ( listing.inqLast6Mths > filters.MAX_INQUIRIES_LAST_6_MONTHS ) {
       return false;
     }
-    if ( ( INCLUDE_LOAN_PURPOSES.length > 0 ) && ( INCLUDE_LOAN_PURPOSES.lastIndexOf( listing.purpose ) === -1 ) )  {
+    if ( ( filters.INCLUDE_LOAN_PURPOSES.length > 0 ) && ( filters.INCLUDE_LOAN_PURPOSES.lastIndexOf( listing.purpose ) === -1 ) )  {
       return false;
     }
-    if ( EXCLUDE_PUBLIC_RECORDS && ( listing.pubRec > 0 ) ) {
+    if ( filters.EXCLUDE_PUBLIC_RECORDS && ( listing.pubRec > 0 ) ) {
       return false;
     }
-    if ( listing.delinq2Yrs > MAX_DELINQUENCIES_LAST_2_YEARS ) {
+    if ( listing.delinq2Yrs > filters.MAX_DELINQUENCIES_LAST_2_YEARS ) {
       return false;
     }
-    if ( ( listing.openAcc > MAX_OPEN_CREDIT_LINES ) || ( listing.openAcc < MIN_OPEN_CREDIT_LINES ) ) {
+    if ( ( listing.openAcc > filters.MAX_OPEN_CREDIT_LINES ) || ( listing.openAcc < filters.MIN_OPEN_CREDIT_LINES ) ) {
       return false;
     }
-    if ( REQUIRE_REVOLVING_BALANCE_LESS_THAN_LOAN_AMOUNT && ( listing.revolBal < listing.loanAmount ) ) {
+    if ( filters.REQUIRE_REVOLVING_BALANCE_LESS_THAN_LOAN_AMOUNT && ( listing.revolBal < listing.loanAmount ) ) {
       return false;
     }
     return true;
@@ -191,6 +185,9 @@ function filterListings( lendingData ) {
   }
 }
 
+// Calls Lending Club's API to order the loans that met your
+// criteria. It will order as many loans as you can afford,
+// prioritizing high yielding loans over lower yielding loans.
 function placeOrder( lendingData ) {
   var deferred = Q.defer();
   var payload = {
@@ -198,18 +195,18 @@ function placeOrder( lendingData ) {
     orders: []
   };
 
-  if ( !PLACE_ORDERS ) {
+  if ( !config.PLACE_ORDERS ) {
     deferred.resolve();
     return deferred.promise;
   }
 
   for ( var i = 0; i < lendingData.loansToOrder.length; i++ ) {
     var loan = lendingData.loansToOrder[ i ];
-    payload.orders.push( { loanId: loan.id, requestedAmount: LOAN_AMOUNT, portfolioId: PORTFOLIO_ID } );
+    payload.orders.push( { loanId: loan.id, requestedAmount: config.LOAN_AMOUNT, portfolioId: PORTFOLIO_ID } );
   }
 
   var options = {
-    url: 'https://api.lendingclub.com/api/investor/' + API_VERS + '/accounts/' + MY_ACCOUNT_ID + '/orders',
+    url: 'https://api.lendingclub.com/api/investor/' + config.API_VERS + '/accounts/' + MY_ACCOUNT_ID + '/orders',
     method: 'POST',
     headers: {
       Authorization: MY_KEY,
@@ -227,7 +224,7 @@ function placeOrder( lendingData ) {
 
   return deferred.promise;
 
-  function orderPlaced( err, response, body ) {
+  function orderPlaced( err, response ) {
     if ( err ) {
       deferred.reject( new Error( 'placeOrder: ' + err ) );
       return;
@@ -240,6 +237,7 @@ function placeOrder( lendingData ) {
   }
 }
 
+// Logs errors to the screen and to file.
 function errorHandler( error ) {
   console.log( error );
   log.error( error );
